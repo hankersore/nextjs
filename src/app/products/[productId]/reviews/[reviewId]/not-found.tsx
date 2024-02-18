56abfb20be63fe5926782a32c6d17f8059fac4f6
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-18 15:58:56 +0500

    catch-all-segments and not-found

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Review is not found</h2>
+        </div>
+    )
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
 export default function ReviewId ({params}:{
     params: {
         productId: string;
         reviewId: string;
     }
 }) {
+    if(parseInt(params.reviewId) > 100) {
+        notFound();
+    }
     return <h1>Review {params.reviewId} for product {params.productId}</h1>
 }
