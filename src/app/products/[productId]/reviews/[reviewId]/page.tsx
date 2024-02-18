5d68d958106860f6e1273b35c2d5a1337cd494a5
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-18 15:28:18 +0500

    all about routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,8 @@
+export default function ReviewId ({params}:{
+    params: {
+        productId: string;
+        reviewId: string;
+    }
+}) {
+    return <h1>Review {params.reviewId} for product {params.productId}</h1>
+}
