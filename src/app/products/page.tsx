2e5f0185ed4486053cdc70ca4fb1990020308c5f
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-20 11:22:04 +0500

    useRouter and usePathname

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 export default function ProductList() {
     return (
         <>
+        <Link href='/'>Home</Link>
         <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <Link href='/products/1'><h2>Product 1</h2></Link>
+        <Link href='/products/2'><h2>Product 2</h2></Link>
+        <Link href='/products/3' replace><h2>Product 3</h2></Link>
         </>
     )
 }
