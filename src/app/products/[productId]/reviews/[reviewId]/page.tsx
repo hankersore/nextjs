f4e04dc311dcc6fe430f720d6655c0a8d2d6b9c1
Dilshoda <alimovadilshoda47@gmail.com>
2024-02-22 16:57:35 +0500

    template, error and error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,20 @@
+'use client';
 import { notFound } from "next/navigation";
+
+function getRandomInt(count: number) {
+    return Math.floor(Math.random()*count);
+}
 export default function ReviewId ({params}:{
     params: {
         productId: string;
         reviewId: string;
     }
 }) {
+//    const random = getRandomInt(2);
+
+//    if (random === 1) {
+//     throw new Error('Error loading review');
+//    }
     if(parseInt(params.reviewId) > 100) {
         notFound();
     }
